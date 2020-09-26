12d0193971c3fae006db92f6255dd3edbc9ac732
Mark Brazee <m.brazee@comcast.net>
2020-09-25 21:19:33 -0600

    working on the value for the prompt length

--- assets/script.js
+++ assets/script.js
@@ -11,36 +11,46 @@ var pwOptions = {
   useUpperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
 }
 
-// Ask user for password options (length and types of characters)
+// Ask user for password length
 
-function generatePassword () {
+function generatePassword() {
   var passwordLength = "";
+  
+  while (true) {
 
+    var length = prompt (" How long do you want your password? Passwords are a minimum of 8 characters and a maximum of 128 characters. ")
+
+    if ( 8 <= length <= 128) break;
+    else continue;
 
   }
+
+  // Create a chracter pool to generate password
+  var characterPool = "";
+
+  characterPool = pwOptions.numChars + pwOptions.useSpecial + pwOptions.useLowercase + pwOptions.useUpperCase;
+
+  // Select and add random characters
+  var UserPassword = "";
+  for (i=0; i , passwordLength; i++) {
+  
+    // Choose a random character
+    var randomIndex = Math.floor(Math.random()* characterPool.length)
+
+    // characters selected by random index numbers from the character pool
+
+    var randomCharacter = characterPool [randomIndex]
+
+    // random characters added to the password
+
+    userPassword = userPassword + randomCharacter;
+  }
+
+  // return password
+
+  return userPassword;
+
 }
-
-// Create a chracter pool to generate password
-var characterPool = "";
-
-characterPool = pwOptions.numChars + pwOptions.useSpecial + pwOptions.useLowercase + pwOptions.useUpperCase;
-
-// Select and add random characters
-var UserPassword = "";
-for (i=0; i , passwordLength; i++) {
-  
-  // Choose a random character
-  var randomIndex = Math.floor(Math.random()* characterPool.length)
-
-  // characters selected by random index numbers from the character pool
-
-  var randomCharacter = characterPool [randomIndex]
-
-  // random characters add to the password
-  userPassword = userPassword + randomCharacter;
-
-  
-
 
 
 
@@ -58,8 +68,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-return result;
-
-
-
-// math.floor(math.random()* 129) + 8;
